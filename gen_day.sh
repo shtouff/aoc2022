14f0a77bf06c19658d23cf9f30a2f1897068001d
Remi Paulmier <remi.paulmier@gmail.com>
2022-12-01 14:54:14 +0100

    add a script for day creation

---
+++ gen_day.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+day=$1 && shift
+
+[[ -d $day ]] && {
+    echo "file exists"
+    exit 1
+}
+
+mkdir ${day}
+mkdir ${day}/s1
+mkdir ${day}/s2
+
+touch ${day}/__init__.py
+touch ${day}/s1/__init__.py
+touch ${day}/s2/__init__.py
+
+cat <<EOF > ${day}/s1/main.py
+#!/usr/bin/env python3
+
+
+EOF
+
+chmod +x ${day}/s1/main.py
+cp ${day}/s1/main.py ${day}/s2/main.py
+
