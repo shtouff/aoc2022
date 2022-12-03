5d95e48ffe16d4e9664f163c726a3e44e734e0d1
Remi Paulmier <remi.paulmier@gmail.com>
2022-12-03 09:09:51 +0100

    optimize ord() ops

--- d03/s1/__init__.py
+++ d03/s1/__init__.py
@@ -1,12 +1,15 @@
 from typing import Set
 
+A = ord('A')
+a = ord('a')
+assert a > A
+
 
 def get_prio(c: str) -> int:
-    o = ord(c)
-    if 65 <= o <= 90:
-        return o - 65 + 27
-    elif 97 <= o <= 122:
-        return o - 97 + 1
+    if (o := ord(c)) >= a:
+        return o - a + 27
+    else:
+        return o - A + 1
 
 
 def get_common_type(*sets: Set[str]) -> str:
