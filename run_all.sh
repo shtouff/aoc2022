5ee75c89cf4cb338a4e748519f4b6597185e3897
Remi Paulmier <remi.paulmier@gmail.com>
2022-12-03 09:53:33 +0100

    shellcheck

--- run_all.sh
+++ run_all.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
-set +x
+set -euo pipefail
 
 for d in $(seq -w 01 25); do
   for s in 1 2; do
     dir="d${d}/s${s}"
     [[ -d "${dir}" ]] && {
       echo -n "${dir}: "
-      (cd ${dir} && PYTHONPATH=$(realpath ../..) ${VIRTUAL_ENV}/bin/python main.py)
+      (cd "${dir}" && PYTHONPATH=$(realpath ../..) "${VIRTUAL_ENV}"/bin/python main.py)
     }
   done
 done
