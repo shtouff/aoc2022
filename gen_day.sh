5a93e24671860ef2c3f88af257386f90b35cff63
Remi Paulmier <remi.paulmier@gmail.com>
2022-12-03 09:57:03 +0100

    shellcheck

--- gen_day.sh
+++ gen_day.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-set -e
-set -o pipefail
+set -euo pipefail
 
 day=$1 && shift
 
@@ -10,20 +9,19 @@ day=$1 && shift
     exit 1
 }
 
-mkdir ${day}
-mkdir ${day}/s1
-mkdir ${day}/s2
+mkdir "${day}"
+mkdir "${day}"/s1
+mkdir "${day}"/s2
 
-touch ${day}/__init__.py
-touch ${day}/s1/__init__.py
-touch ${day}/s2/__init__.py
+touch "${day}"/__init__.py
+touch "${day}"/s1/__init__.py
+touch "${day}"/s2/__init__.py
 
-cat <<EOF > ${day}/s1/main.py
+cat <<EOF > "${day}"/s1/main.py
 #!/usr/bin/env python3
 
 
 EOF
 
-chmod +x ${day}/s1/main.py
-cp ${day}/s1/main.py ${day}/s2/main.py
-
+chmod +x "${day}"/s1/main.py
+cp "${day}"/s1/main.py "${day}"/s2/main.py
